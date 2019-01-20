69ba418089a714fa1567a2ec6bd4d8a90df6879b
Sami Kukkonen <sami@samikukkonen.fi>
2019-01-20 21:07:52 +0200

    Remove ext package

--- packages/ext/src/array.test.ts
+++
@@ -1,7 +0,0 @@
-import "./array"
-import { MonadLaws } from "@bonsai/core"
-import { array, anything } from "fast-check"
-
-describe("Array", () => {
-  MonadLaws.run(Array, array(anything()), anything())
-})
--- packages/ext/src/index.ts
+++
@@ -1,2 +0,0 @@
-import "./array.types"
-import "./array"
